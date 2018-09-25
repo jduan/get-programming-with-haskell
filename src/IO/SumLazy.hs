5f2a6c3e2e8f887259dc6110c3a2f51ea87dcba3
Jingjing Duan <duanjingjing@gmail.com>
2018-09-24 22:52:05 -0700

    interact with lazy IO

---
+++ src/IO/SumLazy.hs
@@ -0,0 +1,11 @@
+module SumLazy where
+
+toInts :: String -> [Int]
+toInts = map read . lines
+
+mainSumLazy :: IO ()
+mainSumLazy = do
+  userInput <- getContents
+  let numbers = toInts userInput
+  let squares = map (^ 2) numbers
+  print (sum squares)
