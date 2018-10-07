2e7777c686274bd686d9adb5c7fdc73788a08a0c
Jingjing Duan <duanjingjing@gmail.com>
2018-10-06 17:43:07 -0700

    Monad Exercises

--- src/Monad/MapLookups.hs
+++ src/Monad/MapLookups.hs
@@ -63,3 +63,30 @@ printDouble n = print (n * 2)
 
 readAndPrintDouble :: IO ()
 readAndPrintDouble = readInt >>= printDouble
+
+askForName :: IO ()
+askForName = putStrLn "What is your name?"
+
+nameStatement :: String -> String
+nameStatement name = "Hello, " ++ name ++ "!"
+
+helloName :: IO ()
+helloName =
+  askForName >> getLine >>= (\name -> return $ nameStatement name) >>= putStrLn
+
+add2 :: Num a => a -> IO a
+add2 n = return (n + 2)
+
+-- These 2 exercises show that Monad is strictly more powerful than Functor
+-- and Applicative.
+--
+allFmapM :: Monad m => (a -> b) -> m a -> m b
+allFmapM f m = f <$> m
+
+allApp :: Monad m => m (a -> b) -> m a -> m b
+allApp mf ma = mf <*> ma
+
+-- Implement bind for Maybe
+bind :: Maybe a -> (a -> Maybe b) -> Maybe b
+bind Nothing _ = Nothing
+bind (Just a) f = f a
